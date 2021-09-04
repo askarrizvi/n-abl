9f07e4b51bb7608fca033321a5cb88233dbacadb
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-09-03 20:28:19 -0400

    Update index.js

--- tweets/index.js
+++ tweets/index.js
@@ -10,8 +10,9 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
