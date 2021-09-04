509fd7144bc71db393dc2ff431f8418a2fa3a68b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-04 15:00:19 -0400

    sentiment

---
+++ sentiment.js
@@ -0,0 +1,2 @@
+var Sentiment = require('sentiment');
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
