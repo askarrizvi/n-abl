9c49ab550de7cc1bc21fac7d422b5dd0bb526827
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-03 20:36:54 -0400

    test update

--- __tests__/Comment.test.js
+++ __tests__/Comment.test.js
@@ -8,3 +8,12 @@ test('creates a comment object', () => {
     expect(comment.user_id).toBe(expect.Number);
     expect(comment.post_id).toBe(expect.Number);
 });
+
+test('checks for values', () => {
+    const comment = new Comment({id: 1, comment_text: 'This is a test', user_id: 1, post_id: 1});
+    
+    expect(comment.id).toBe(1);
+    expect(comment.comment_text).toBe('This is a test');
+    expect(comment.user_id).toBe(1);
+    expect(comment.post_id).toBe(1);
+});
--- __tests__/Post.test.js
+++ __tests__/Post.test.js
@@ -1,11 +1,20 @@
 const Post = require('../models/post');
 
 
-test('creates a user object', () => {
+test('creates a post object', () => {
     const post = new Post();
   
     expect(post.id).toBeNull;
     expect(post.title).toBe(expect.String);
     expect(post.post_url).toBe(expect.String);
     expect(post.user_id).toBe(expect.Number);
+});
+
+test('checks for values', () => {
+    const post = new Post({id: 1, title: 'Test', post_url: 'test.com', user_id: 1});
+  
+    expect(post.id).toBe(1);
+    expect(post.title).toBe('Test');
+    expect(post.post_url).toBe('test.com');
+    expect(post.user_id).toBe(1);
 });
