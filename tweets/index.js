ed6189383c7178f1219b0b9818958f252143f06c
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-09-03 20:23:41 -0400

    Update index.js

--- tweets/index.js
+++ tweets/index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 const io = require('socket.io')(80);
 const cfg = require('./config.json');
 const tw = require('node-tweet-stream')(cfg);
