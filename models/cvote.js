2458480b9ddb06ae511f82c4ee7bd4639c12d7ab
d3guzman-dennis <84108180+d3guzman-dennis@users.noreply.github.com>
2021-09-05 13:16:25 -0400

    Updated comment vote js

--- models/cvote.js
+++ models/cvote.js
@@ -1,1 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class CVote extends Model {}
+
+CVote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    comment_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'comment',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = CVote;
