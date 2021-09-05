ddd23ed466d79c6dee7d3c1224961d908028e389
d3guzman-dennis <84108180+d3guzman-dennis@users.noreply.github.com>
2021-09-05 14:43:33 -0400

    Update tags.js

--- models/tags.js
+++ models/tags.js
@@ -1,1 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class Tags extends Model {}
+
+Tags.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 10]
+      }
+    },
+    comment_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'comment',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = Tags;
